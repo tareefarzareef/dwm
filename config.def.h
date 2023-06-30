a43b5463cc6722b58084048fceef254a62a759a6
tareefarzareef <thetareefarz@gmail.com>
2023-06-30 23:13:44 +0600

    bozonic commit

--- config.def.h
+++ config.def.h
@@ -10,14 +10,13 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showsystray        = 1;        /* 0 means no systray */ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cascadia Code:size=10", "JoyPixels:size=10"};
-static const char dmenufont[]       = "Cascadia Code:size=10";
+static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=10", "JoyPixels:size=10"};
+static const char dmenufont[]       = "CaskaydiaCove Nerd Font:size=10";
 static const char col_gray1[]       = "#363636"; // Bar Background
 static const char col_gray2[]       = "#660d0d"; // Unselected Window Border
-static const char col_gray3[]       = "#dddddd"; // Unselected Font Color Tag
+static const char col_gray3[]       = "#bbbbbb"; // Unselected Font Color Tag
 static const char col_gray4[]       = "#FFFFFF"; // Selected font color
 static const char col_cyan[]        = "#b51919"; // Selected window border + selected tag + name
 static const char *colors[][3]      = {
@@ -77,6 +76,7 @@ static const char *brightnessup[] = { "xbacklight", "-inc", "5", NULL } ;
 static const char *brightnessdown[] = { "xbacklight", "-dec", "5", NULL } ;
 static const char *scrotcmd[] = { "scrot", "-p", NULL } ;
 static const char *sleepcmd[] = { "systemctl", "suspend", NULL } ;
+static const char *codecmd[] = { "code", NULL } ;
 
 
 static const Key keys[] = {
@@ -93,6 +93,7 @@ static const Key keys[] = {
 	{ MODKEY,                  	XK_s,     spawn,          {.v = scrotcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_v, spawn,          {.v = codecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
