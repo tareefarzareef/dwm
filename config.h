36abd8e34cf939f6439ff735f1de7c00dff8f4bc
tareefarzareef <thetareefarz@gmail.com>
2023-07-07 15:32:04 +0600

    Added Bar Height Patch

--- config.h
+++ config.h
@@ -4,17 +4,17 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */ static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 23;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 #define ICONSIZE 16   /* icon size */
-#define ICONSPACING 5 /* space between icon and title */
+#define ICONSPACING 7 /* space between icon and title */
 static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=10", "JoyPixels:size=10"};
-static const char dmenufont[]       = "CaskaydiaCove Nerd Font:size=10";
 static const char col_gray1[]       = "#363636"; // Bar Background
 static const char col_gray2[]       = "#660d0d"; // Unselected Window Border
 static const char col_gray3[]       = "#bbbbbb"; // Unselected Font Color Tag
